cb4e54331d7f78ee9b8a105cbbe3fa0365798259
AndrewHack242 <andrew.hack242@gmail.com>
2024-08-26 20:49:54 -0600

    Add project files.

---
+++ Start_with_Fatality_Board/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Start_with_Fatality_Board
+{
+    [BepInPlugin(mod_GUID, mod_name, mod_version)]
+    public class Start_with_fatality_board_base : BaseUnityPlugin
+    {
+        private const string mod_GUID = "Hackattack242.Start_with_Fatality_Board";
+        private const string mod_name = "Start_with_Fatality_Board";
+        private const string mod_version = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(mod_GUID);
+
+        private static Start_with_fatality_board_base Instance;
+
+        private ManualLogSource logger;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(mod_name);
+
+            logger.LogInfo(mod_name + " has awoken");
+
+            harmony.PatchAll();
+
+            logger.LogInfo("Patches Complete");
+        }
+
+        internal static void LogDebug(string message)
+        {
+            Instance.Log(message, (LogLevel)32);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            Instance.Log(message, (LogLevel)16);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Instance.Log(message, (LogLevel)4);
+        }
+
+        internal static void LogError(string message)
+        {
+            Instance.Log(message, (LogLevel)2);
+        }
+
+        internal static void LogError(Exception ex)
+        {
+            Instance.Log(ex.Message + "\n" + ex.StackTrace, (LogLevel)2);
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            ((Start_with_fatality_board_base)this).logger.Log(logLevel, (object)message);
+        }
+
+    }
+}
---
+++ Start_with_Fatality_Board/Patches/Board_Patch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace Start_with_Fatality_Board.Patches
+{
+    internal class Board_Patch
+    {
+        [HarmonyPatch(typeof(StartOfRound))]
+        internal class NewSaveStartPatch
+        {
+            [HarmonyPatch("firstDayAnimation")]
+            [HarmonyPostfix]
+            internal static void LoadTelevisionFromConfig()
+            {
+                if (StartOfRound.Instance.gameStats.daysSpent == 0 && !StartOfRound.Instance.isChallengeFile)
+                {
+                    Start_with_fatality_board_base.LogInfo("New save detected, unlocking Fatalities Sign.");
+                }
+                else
+                {
+                    Start_with_fatality_board_base.LogInfo("Not a new save, not unlocking Fatalities Sign.");
+                    return;
+                }
+
+                // check if the player is a host, if not return
+                if (!GameNetworkManager.Instance.isHostingGame) return;
+
+                List<UnlockableItem> unlockablesList = StartOfRound.Instance.unlockablesList.unlockables;
+
+                // Search for Fatalities Sign in unlockables list
+                foreach (UnlockableItem unlockable in unlockablesList)
+                {
+                    var unlockableName = unlockable.unlockableName;
+                    var unlockableID = unlockablesList.IndexOf(unlockable);
+
+                    Start_with_fatality_board_base.LogDebug($"Checking unlockableName {unlockableName} and ID {unlockableID}.");
+
+                    var televisionResult = CheckConfig(unlockableID, unlockableName);
+
+                    if (televisionResult) break;
+                }
+            }
+
+            private static bool CheckConfig(int unlockableID, string unlockableName)
+            {
+                if (unlockableName.Equals("Fatalities Sign"))
+                {
+                    Start_with_fatality_board_base.LogInfo($"Found Fatalities Sign with unlockableName {unlockableName} and ID {unlockableID}. Unlocking.");
+                    UnlockShipItem(StartOfRound.Instance, unlockableID, unlockableName);
+                    return true;
+                }
+                return false;
+            }
+
+            private static void UnlockShipItem(StartOfRound instance, int unlockableID, string name)
+            {
+                try
+                {
+                    Start_with_fatality_board_base.LogInfo($"Attempting to unlock {name}");
+                    var unlockShipMethod = instance.GetType().GetMethod("UnlockShipObject",
+                        System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    unlockShipMethod.Invoke(instance, new object[] { unlockableID });
+                    Start_with_fatality_board_base.LogInfo($"Spawning {name}");
+                }
+                catch (NullReferenceException ex)
+                {
+                    Start_with_fatality_board_base.LogError($"Could not invoke UnlockShipObject method: {ex}");
+                }
+            }
+        }
+    }
+}
